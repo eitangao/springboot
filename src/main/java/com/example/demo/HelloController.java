b7115d23a12e81a4354b0eec2ddc8b55dbf20f62
egao035 <eitan.y.gao@pwc.com>
2017-12-12 10:57:02 +0800

    modify the application.properties

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping(value="/hello")
     public String hello(){
-        return "hello,Spring Boot";
+        return "hello,Spring Boot ,test hot deployment ";
     }
 }
 
