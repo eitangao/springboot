10d5707455e439f24eda5a3d9a943cb26e70f4d1
egao035 <eitan.y.gao@pwc.com>
2017-12-20 00:16:04 +0800

    add sql,create java calsses

---
+++ createUser.sql
@@ -0,0 +1,12 @@
+drop table if exists user;
+create table user(
+id int(10)not null auto_increment comment'ID',
+user_name varchar(20) not null unique comment'用户名',
+password varchar(20) not null comment'密码',
+create_time datetime not null default current_timestamp comment'创建时间',
+primary key(id));
+insert into user(user_name,password) values('user1','123'),
+('user2','123'),
+('trump','123'),
+('obama','123');
+
